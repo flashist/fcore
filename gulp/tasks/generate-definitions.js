d437d533f45ce078383640b9f05ff370efecdd81
Mark Dolbyrev <mark.dolbyrev@lcsas-MacBook-Pro-2.local>
2016-08-22 18:59:24 +0300

    Modules refactoring (moving the core/graphics/console modules to separate repos).

--- gulp/tasks/generate-definitions.js
+++ gulp/tasks/generate-definitions.js
@@ -36,11 +36,11 @@ gulp.task(
                 var processFile = function(file, cb) {
 
                     var importPath = path.relative(basePath, file.path);
-                    /*if (importPath.indexOf(".d.ts") != -1) {
+                    if (importPath.indexOf(".d.ts") != -1) {
                         importPath = importPath.substr(0, importPath.lastIndexOf(".d.ts"));
                     }else if (importPath.indexOf(".ts") != -1) {
                         importPath = importPath.substr(0, importPath.lastIndexOf(".ts"));
-                    }*/
+                    }
                     console.log(importPath);
 
                     resultDeclarationText += "export * from '" + "./" + importPath + "'";
