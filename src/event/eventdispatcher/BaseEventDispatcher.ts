da56d1bf3655828040fa44a657bd87307db40228
Mark Dolbyrev <mark.dolbyrev@lcsas-MacBook-Pro-2.local>
2017-05-16 20:21:21 +0300

    Changing of the resource loading classes.

--- src/event/eventdispatcher/BaseEventDispatcher.ts
+++ src/event/eventdispatcher/BaseEventDispatcher.ts
@@ -5,32 +5,32 @@ import * as EventEmitter from "eventemitter3";
 
 // Type definition for the eventemitter3
 declare type IEventEmitter = {
-    addListener(type:string, listener:Function, context?:any);
-    removeListener(type:string, listener:Function, context?:any);
-    removeAllListeners(type?:string);
-    emit(type:string, ...args);
+    addListener(type: string, listener: Function, context?: any);
+    removeListener(type: string, listener: Function, context?: any);
+    removeAllListeners(type?: string);
+    emit(type: string, ...args);
 };
 
 export class BaseEventDispatcher implements IEventDispatcher<string> {
-    private eventEmitter:IEventEmitter;
+    private eventEmitter: IEventEmitter;
 
     public constructor() {
         this.eventEmitter = (new EventEmitter() as IEventEmitter);
     }
 
-    addEventListener(type:string, listener:IEventListenerCallback<string>):void {
+    addEventListener(type: string, listener: IEventListenerCallback<string>): void {
         this.eventEmitter.addListener(type, listener as Function);
     }
 
-    removeAllEventListeners(type?:string):void {
+    removeAllEventListeners(type?: string): void {
         this.eventEmitter.removeAllListeners(type);
     }
 
-    removeEventListener(type:string, listener:any):void {
+    removeEventListener(type: string, listener: IEventListenerCallback<string>): void {
         this.eventEmitter.removeListener(type, listener as Function);
     }
 
-    dispatchEvent(event:string, ...args:any[]):void {
+    dispatchEvent(event: string, ...args: any[]): void {
         this.eventEmitter.emit(event, ...args);
     }
 }
