ea4ffc211e5b14c39e7fddae769ba1553a64fc7a
Mark Dolbyrev <mark.dolbyrev@gmail.com>
2018-01-12 23:24:56 +0300

    Editor Page: in progress.

--- src/datastructure/Dictionary.ts
+++ src/datastructure/Dictionary.ts
@@ -1,22 +1,27 @@
 ﻿import {UniqueTools} from "../tools/UniqueTools";
 export class Dictionary<KeyType, ItemType> {
 
-    protected map:any;
+    protected map: any;
+    protected keys: KeyType[];
 
     public constructor() {
         this.map = {};
+        this.keys = [];
     }
 
-
-    public getItem(key:KeyType):ItemType {
-        var tempId:string = UniqueTools.getObjectUniqueId(key);
-
-        var result:any = this.map[tempId];
-        return result;
+    public getItem(key: KeyType): ItemType {
+        var tempId: string = UniqueTools.getObjectUniqueId(key);
+        return this.map[tempId];
     }
 
-    public addItem(key:KeyType, item:ItemType):void {
-        var tempId:string = UniqueTools.getObjectUniqueId(key);
+    public addItem(key: KeyType, item: ItemType): void {
+        var tempId: string = UniqueTools.getObjectUniqueId(key);
         this.map[tempId] = item;
+
+        this.keys.push(key);
+    }
+
+    public getKeys(): KeyType[] {
+        return this.keys.concat();
     }
 }
