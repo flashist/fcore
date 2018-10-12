e5733c10e480567056faafb3b379ac84a3fa6f85
Mark Dolbyrev <mark.dolbyrev@gmail.com>
2018-10-12 20:39:31 +0300

    Shared libraries improvement.

--- src/tools/NumberTools.ts
+++ src/tools/NumberTools.ts
@@ -7,8 +7,8 @@
      */
     public static MAX_SAFE_INTEGER: number = (Number as any).MAX_SAFE_INTEGER ? (Number as any).MAX_SAFE_INTEGER : Math.pow(2, 53) - 1;
 
-    public static getRandom(min:number, max:number, isFloor:boolean = false, isRound:boolean = false, isCeil:boolean = false):number {
-        var result:number = min + Math.random() * (max - min);
+    public static getRandom(min: number, max: number, isFloor: boolean = false, isRound: boolean = false, isCeil: boolean = false): number {
+        var result: number = min + Math.random() * (max - min);
 
         if (isFloor) {
             result = Math.floor(result);
@@ -18,6 +18,47 @@
         }
         if (isCeil) {
             result = Math.ceil(result);
+        }
+
+        return result;
+    }
+
+    public static getListOfIntegers(from: number, to: number): number[] {
+        let result: number[] = [];
+
+        from = Math.floor(from);
+        to = Math.floor(to);
+
+        if (from < to) {
+            for (let index: number = from; index <= to; index++) {
+                result.push(index);
+            }
+
+        } else {
+            console.error("NumberTools | GetListOfIntegers __ WARNING! variable 'from' is not less than 'to'");
+        }
+
+        return result;
+    }
+
+    public static roundTo(
+        source: number,
+        roundTo: number = 10,
+        isFloor: boolean = false,
+        isRound: boolean = true,
+        isCeil: boolean = false): number {
+
+        let result: number = source;
+
+        let tempKoef: number = 1 / roundTo;
+        if (isFloor) {
+            result = Math.floor(source * tempKoef) / tempKoef;
+        }
+        if (isRound) {
+            result = Math.round(source * tempKoef) / tempKoef;
+        }
+        if (isCeil) {
+            result = Math.ceil(source * tempKoef) / tempKoef;
         }
 
         return result;
