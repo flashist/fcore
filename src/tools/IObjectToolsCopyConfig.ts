de92f4dd10d54fb1fd63c29c2274cd82764cfbf1
Mark Dolbyrev <mark.dolbyrev@gmail.com>
2020-09-14 13:49:19 +0300

    In progress.

--- src/tools/IObjectToolsCopyConfig.ts
+++ src/tools/IObjectToolsCopyConfig.ts
@@ -1,4 +1,8 @@
 export interface IObjectToolsCopyConfig {
     ignoreExistedProperties?: boolean,
+    // If there is already a property in the target object,
+    // then this property would be overridden
+    // (e.g. if it was an object, the object would be overridden, instead of copying separate properties into it)
+    // Might be useful in cases when you need to override non-primitive objects
     overrideExistedProperties?: boolean
 }
