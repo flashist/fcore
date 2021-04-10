db73b2eec6d057e039412788a270684cc72436fe
Mark Dolbyrev <mark.dolbyrev@gmail.com>
2021-04-10 18:57:59 +0300

    Preparing the IConstructor class for generics.

--- src/other/IConstructor.ts
+++ src/other/IConstructor.ts
@@ -1,4 +1,4 @@
 export interface IConstructor<T extends new (...args: any) => any> {
     // we can use built-in InstanceType to infer instance type from class type
-    type: new (...args: ConstructorParameters<T>) => InstanceType<T>;
+    new (...args: ConstructorParameters<T>): InstanceType<T>;
 }
