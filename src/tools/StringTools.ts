04c5e8a5d22ed884a7c16db8bccc20fabe37e6db
Mark Dolbyrev <mark.dolbyrev@gmail.com>
2022-11-23 16:26:18 +0300

    Lib update

--- src/tools/StringTools.ts
+++ src/tools/StringTools.ts
@@ -1,23 +1,23 @@
 
-import {ObjectTools} from "./ObjectTools";
+import { ObjectTools } from "./ObjectTools";
 
 export class StringTools {
 
-    public static substituteList(sourceText:string, ...args):string {
+    public static substituteList(sourceText: string, ...args): string {
         return StringTools.substitute(sourceText, args);
     }
 
-    public static substitute(sourceText:string, substituteParams:any = null):string {
-        var resultStr:string = sourceText;
+    public static substitute(sourceText: string, substituteParams: any = null): string {
+        var resultStr: string = sourceText;
 
         if (ObjectTools.isSimpleType(substituteParams)) {
             substituteParams = [substituteParams];
         }
 
         if (substituteParams) {
-            var tempArgStr:string;
-            var foundPattern:string;
-            var paramName:string;
+            var tempArgStr: string;
+            var foundPattern: string;
+            var paramName: string;
             for (paramName in substituteParams) {
                 tempArgStr = <string>substituteParams[paramName];
 
@@ -29,9 +29,9 @@ export class StringTools {
         return resultStr;
     }
 
-    public static replaceText(sourceString:string, searchString:string, replaceString:string):string {
+    public static replaceText(sourceString: string, searchString: string, replaceString: string): string {
         // Split the string (if the search string has been found)
-        var replacedString:string = sourceString.split(searchString).join(replaceString);
+        var replacedString: string = sourceString.split(searchString).join(replaceString);
 
         return replacedString;
     }
@@ -65,4 +65,14 @@ export class StringTools {
         const result: string = resultArr.join("");
         return result;
     }
+
+    public static splitByCapitalLetters(source: string): string[] {
+        return source.split(/(?=[A-Z])/);
+    }
+
+    public static capitalizeFirstLetter(source: string, locale: string = navigator.language): string {
+        return source.charAt(0).toLocaleUpperCase(locale) + source.substring(1);
+    }
+
+
 }
