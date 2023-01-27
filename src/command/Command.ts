92942f5debfa9128737840d9690ee031b132c19a
Mark Dolbyrev <mark.dolbyrev@gmail.com>
2023-01-27 21:13:20 +0300

    Update

--- src/command/Command.ts
+++ src/command/Command.ts
@@ -23,6 +23,7 @@ export abstract class Command<ResolveType = any> extends BaseObject {
     }
 
     public errorCode: string;
+    public rejectErrorData: any;
 
     protected executePrommise: Promise<ResolveType>;
     private promiseResolve: (result?: ResolveType) => void;
@@ -92,15 +93,18 @@ export abstract class Command<ResolveType = any> extends BaseObject {
 
             this._isCompleted = true;
 
+            this.rejectErrorData = rejectErrorData;
+
             this.removeListeners();
 
             this.dispatchEvent(CommandEvent.COMPLETE);
 
-            if (this.success) {
-                this.promiseResolve(resolveData);
-            } else {
-                this.promiseReject(rejectErrorData);
-            }
+            // if (this.success) {
+            //     this.promiseResolve(resolveData);
+            // } else {
+            //     this.promiseReject(rejectErrorData);
+            // }
+            this.promiseResolve(resolveData);
         }
 
         //
