48e2ff43055046a0b5285040c1302981f7bfdc3a
Mark Dolbyrev <ruflashist@gmail.com>
2026-01-15 16:03:03 +0300

    Update

--- src/tools/NumberTools.ts
+++ src/tools/NumberTools.ts
@@ -110,4 +110,8 @@
 
         return result;
     }
+
+    static getDistance(x1: number, y1: number, x2: number, y2: number): number {
+        return Math.hypot(Math.abs(x1 - x2), Math.abs(y1 - y2));
+    };
 }
